7dd330f1465d41ae500c8450d7e3b1b991829770
Chanawee <chanawee.janyakhantikul@gmail.com>
2020-11-22 18:34:22 +0700

    FixBug

--- src/index.js
+++ src/index.js
@@ -1,18 +1,41 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import { createBrowserHistory } from "history";
+import { Router, Route, Switch, Redirect } from "react-router-dom";
+import db from "./firebase";
 import { Provider } from "react-redux";
 import { store } from "redux/store";
 
+import "bootstrap/dist/css/bootstrap.css";
+import "assets/scss/paper-dashboard.scss?v=1.2.0";
+import "assets/demo/demo.css";
+import "perfect-scrollbar/css/perfect-scrollbar.css";
+
+import AdminLayout from "layouts/Admin.js";
+
+const hist = createBrowserHistory();
+
 store.subscribe(() => {
-  alert('Updated')
+  alert("Updated");
   console.log("update store", store.getState());
+});
+
+db.on("value", (data) => {
+  if (data.val()) {
+    store.dispatch({ type: "addGraph", payload: data.val()["GraphData"] });
+    store.dispatch({ type: "addRoom", payload: data.val()["Room"] });
+  }
 });
 
 ReactDOM.render(
   <React.Fragment>
     <Provider store={store}>
-      <App />
+      <Router history={hist}>
+        <Switch>
+          <Route path="/admin" render={(props) => <AdminLayout {...props} />} />
+          <Redirect to="/admin/dashboard" />
+        </Switch>
+      </Router>
     </Provider>
   </React.Fragment>,
   document.getElementById("root")
