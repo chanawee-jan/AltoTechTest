e975a5eaa138fc32c6d36d962158a32440dc2d1d
Chanawee <chanawee.janyakhantikul@gmail.com>
2020-11-22 19:53:02 +0700

    FixBug

--- gulpfile.js
+++ gulpfile.js
@@ -8,16 +8,13 @@ gulp.task("licenses", async function () {
     .pipe(
       gap.prependText(`/*!
 
-=========================================================
 * Paper Dashboard React - v1.2.0
-=========================================================
 
 * Product Page: https://www.creative-tim.com/product/paper-dashboard-react
 * Copyright 2020 Creative Tim (http://www.creative-tim.com)
 
 * Coded by Creative Tim
 
-=========================================================
 
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
@@ -31,16 +28,13 @@ gulp.task("licenses", async function () {
     .pipe(
       gap.prependText(`<!--
 
-=========================================================
 * Paper Dashboard React - v1.2.0
-=========================================================
 
 * Product Page: https://www.creative-tim.com/product/paper-dashboard-react
 * Copyright 2020 Creative Tim (http://www.creative-tim.com)
 
 * Coded by Creative Tim
 
-=========================================================
 
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
@@ -54,16 +48,13 @@ gulp.task("licenses", async function () {
     .pipe(
       gap.prependText(`/*!
 
-=========================================================
 * Paper Dashboard React - v1.2.0
-=========================================================
 
 * Product Page: https://www.creative-tim.com/product/paper-dashboard-react
 * Copyright 2020 Creative Tim (http://www.creative-tim.com)
 
 * Coded by Creative Tim
 
-=========================================================
 
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
